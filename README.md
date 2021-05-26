c7535de498c1ebe8259fa8b227216f2b8e0d69ae
doyeonlee <leehr7603@naver.com>
2021-05-26 12:14:36 +0900

    0526.modify

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # 리액트 시작하기
+
+## 리액트 앱 만들기
+
+npx create-react-app 앱이름
+
+## 실행하기
+
+cd react 앱이름
+npm start
+
+## 로우터 설정
+
+npm install react-router-dom
+
+## 변수 타입 설정
+
+npm install prop-types
+
+## SASS 설정
+
+npm install node-sass
--- src/index.js
+++ src/index.js
@@ -1,38 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.scss";
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function Comment(props) {
-  return (
-    <div className="comment">
-      <div className="user-info">
-        <img
-          className="avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
-        <div className="user-name">{props.author.name}</div>
-      </div>
-      <div className="comment-text">{props.text}</div>
-      <div className="comment-date">{formatDate(props.date)}</div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: "텍스트",
-  author: {
-    name: "paperbag",
-    avatarUrl:
-      "http://studydoyeon.dothome.co.kr/portfolio/assets/images/aboutImg02@2x.jpg",
-  },
-};
-
-ReactDOM.render(
-  <Comment date={comment.date} text={comment.text} author={comment.author} />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
